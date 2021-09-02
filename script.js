47b8f9eadbcb79e6e8722278997fe5d0a8fba28a
Imaad Uddin <imaaduddin@icloud.com>
2021-09-02 13:03:52 -0500

    finished styling, adding functionality

---
+++ script.js
@@ -0,0 +1,41 @@
+const video = document.querySelector("video");
+const progressRange = document.querySelector(".progress-range");
+const progressBar = document.querySelector(".progress-bar");
+const playBtn = document.getElementById("play-btn");
+const volumeIcon = document.getElementById("volume-icon");
+const volumeRange = document.querySelector(".volume-range");
+const volumeBar = document.querySelector(".volume-bar");
+const currentTime = document.querySelector(".time-elapsed");
+const duration = document.querySelector(".time-duration");
+const fullscreenBtn = document.querySelector(".fullscreen");
+
+// Play & Pause 
+function showPlayIcon() {
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+}
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+}
+
+// On Video End, Show Play Icon 
+video.addEventListener("ended", showPlayIcon);
+
+// Update Progress Bar As Video Plays 
+function updateProgress() {
+  console.log("currentTime", video.currentTime, "duration", video.duration);
+}
+
+// Event Listeners 
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canPlay", updateProgress);

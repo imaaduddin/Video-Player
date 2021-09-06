8d794723222ff7c56e8eb7307801560df52ab43d
Imaad Uddin <imaaduddin@icloud.com>
2021-09-06 17:40:34 -0500

    displaying the time

--- script.js
+++ script.js
@@ -29,9 +29,19 @@ function togglePlay() {
 // On Video End, Show Play Icon 
 video.addEventListener("ended", showPlayIcon);
 
+// Calculate display time format 
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+
 // Update Progress Bar As Video Plays 
 function updateProgress() {
-  console.log("currentTime", video.currentTime, "duration", video.duration);
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
 }
 
 // Event Listeners 
